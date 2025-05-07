89662ceceadbec035fb8b63e19fe185102646786
kri-bak <kriba@aarhus.dk>
2025-05-07 14:08:52 +0200

    Fixed SQL comment

--- robot_framework/custom/sql_data.py
+++ robot_framework/custom/sql_data.py
@@ -29,7 +29,7 @@ WHERE current_address.Komkode = '0751' -- Aarhus Kommune
     AND current_address.DatoTilflyt > Convert(datetime, '{from_date}')
     AND current_address.DatoTilflyt < Convert(datetime, '{to_date}')
     AND current_address.DatoFraflytAAK IS NULL
--- Only look at those whose previous address is in Denmark
+-- Only look at those whose previous address is NOT in Denmark
     AND previous_address.Komkode NOT IN ('0751', '0101' ,'0147' ,'0151' ,'0153' ,'0155' ,'0157' ,'0159' ,'0161' ,'0163' ,'0165' ,'0167' ,'0169' ,'0173' ,'0175' ,'0183' ,'0185' ,'0187' ,'0190' ,'0201' ,'0210' ,'0217' ,'0219' ,'0223' ,'0230' ,'0240' ,'0250' ,'0253' ,'0259' ,'0260' ,'0265' ,'0269' ,'0270' ,'0306' ,'0316' ,'0320' ,'0326' ,'0329' ,'0330' ,'0336' ,'0340' ,'0350' ,'0360' ,'0370' ,'0376' ,'0390' ,'0400' ,'0410' ,'0411' ,'0420' ,'0430' ,'0440' ,'0450' ,'0461' ,'0479' ,'0480' ,'0482' ,'0492' ,'0510' ,'0530' ,'0540' ,'0550' ,'0561' ,'0563' ,'0573' ,'0575' ,'0580' ,'0607' ,'0615' ,'0621' ,'0630' ,'0657' ,'0661' ,'0665' ,'0671' ,'0706' ,'0707' ,'0710' ,'0727' ,'0730' ,'0740' ,'0741' ,'0746' ,'0756' ,'0760' ,'0766' ,'0773' ,'0779' ,'0787' ,'0791' ,'0810' ,'0813' ,'0820' ,'0825' ,'0840' ,'0846' ,'0849' ,'0851' ,'0860') -- Danish commune codes
     AND previous_address.Vejkode NOT IN ('9902', '9901') -- Homeless and couch surfers
 -- Sort on data from address database
